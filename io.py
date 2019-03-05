b3557669afeeaa2acf2b07699822cb6d89dd4930
Johan Ullen <johan.ullen@sentian.ai>
2019-03-05 18:30:59 +0100

    read file

---
+++ io.py
@@ -0,0 +1,11 @@
+
+
+def read(filename):
+    with open(filename, "r") as f:
+        pics = list()
+        for ix, line in enumerate(f):
+            line = line.split()
+            orientation = line[0]
+            tags = line[2:]
+            pics.append((ix, orientation, tags))
+    return pics
