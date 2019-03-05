0a58c111306c733ac9bf1c53ebbfb20746102e2b
Johan Ullen <johan.ullen@sentian.ai>
2019-03-05 19:37:36 +0100

    naive_union bugfix, max_intersection join

--- orientation.py
+++ orientation.py
@@ -9,21 +9,44 @@ def split(pics):
 def naive_union(verts):
     new_verts = list()
     for ix, _ in enumerate(verts[:-1:2]):
+        ix *= 2
+        print(ix)
         a, b = verts[ix:ix+2]
         idx = (a[0][0], b[0][0])
         orient = a[1]
         tags = a[2] | b[2]
         new_verts.append((idx, orient, tags))
     return new_verts
-    # return [((a[0], b[0]), a[1], a[2] | b[2]) for a, b in verts[::2]]
 
 
-def min_union(verts):
-    pass
+def max_inter(verts):
+    verts = verts.copy()
+    new_verts = list()
+    while verts:
+        a = verts.pop()
+
+        max_i = None
+        b = None
+        for test_b in verts:
+            size_i = len(a[2] & test_b[2])
+            if b is None or size_i > max_i:
+                b = test_b
+                max_i = size_i
+        if b is None:
+            continue
+
+        verts.remove(b)
+        idx = (a[0][0], b[0][0])
+        orient = a[1]
+        tags = a[2] | b[2]
+        new_verts.append((idx, orient, tags))
+    return new_verts
 
 
-# from io_hashcode import read
-# pics = read("/home/ju/Downloads/2019/a_example (copy).txt")
-# verts, horts = split(pics)
-# verts = naive_union(verts)
-# print(verts)
+from io_hashcode import read
+pics = read("/home/ju/Downloads/2019/a_example (copy).txt")
+verts, horts = split(pics)
+verts_ = naive_union(verts)
+print(verts_)
+verts_ = max_inter(verts)
+print(verts_)
